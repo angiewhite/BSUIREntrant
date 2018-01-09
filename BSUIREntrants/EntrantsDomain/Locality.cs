fe5155f4cfbf59f0360563fac729ef6304ba0119
Angelina Belyasova <belyasova98@mail.ru>
2018-01-09 14:02:47 +0300

    Drop-downs & bug fix

---
+++ BSUIREntrants/EntrantsDomain/Locality.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntrantsDomain
+{
+    public class Locality
+    {
+        public int LocalityId { get; set; }
+        public string LocalityName { get; set; }
+        public int RegionId { get; set; }
+        public Region Region { get; set; }
+    }
+}
---
+++ BSUIREntrants/EntrantsDomain/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntrantsDomain
+{
+    public class Region
+    {
+        public int RegionId { get; set; }
+        public string RegionalCenter { get; set; }
+    }
+}
